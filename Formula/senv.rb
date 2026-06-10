81f200a475ef6c2853dd349434e61fb33d0e933a
Alan <tae.gun7784@gmail.com>
2026-06-11 01:06:28 +0900

    Fix formula license DSL
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- Formula/senv.rb
+++ Formula/senv.rb
@@ -3,7 +3,7 @@ class Senv < Formula
   homepage "https://github.com/gtg7784/senv"
   url "https://github.com/gtg7784/senv/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "13e4abfe66265ce0b32ce75fdc0af18aa7f7f9b90adcb8f5f8f8b6ae1960a398"
-  license "MIT OR Apache-2.0"
+  license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/gtg7784/senv.git", branch: "main"
 
   depends_on "rust" => :build
