a425146b015443bec09242f2206c75f8030d0ab6
Alan <tae.gun7784@gmail.com>
2026-06-11 01:05:14 +0900

    Add senv formula
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ Formula/senv.rb
@@ -0,0 +1,18 @@
+class Senv < Formula
+  desc "Encrypted .env replacement with first-class TUI"
+  homepage "https://github.com/gtg7784/senv"
+  url "https://github.com/gtg7784/senv/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "13e4abfe66265ce0b32ce75fdc0af18aa7f7f9b90adcb8f5f8f8b6ae1960a398"
+  license "MIT OR Apache-2.0"
+  head "https://github.com/gtg7784/senv.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "senv #{version}", shell_output("#{bin}/senv --version")
+  end
+end
